a1e5ed9db6a796407f7edefbe664079b4bfae397
nolte <nolte07@googlemail.com>
2020-01-28 15:10:46 +0100

    add outputs

--- helm/set_facts/setfacts.sh
+++ helm/set_facts/setfacts.sh
@@ -9,3 +9,7 @@ CHARTNAME=$(yq r ${CHART_YAML} name)
 echo ::set-env name=CHARTVERSION::"${CHARTVERSION}"
 echo ::set-env name=CHARTAPPVERSION::"${CHARTAPPVERSION}"
 echo ::set-env name=CHARTNAME::"${CHARTNAME}"
+
+echo ::set-output name=CHARTVERSION::"${CHARTVERSION}"
+echo ::set-output name=CHARTAPPVERSION::"${CHARTAPPVERSION}"
+echo ::set-output name=CHARTNAME::"${CHARTNAME}"
