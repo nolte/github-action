9fc648cdaf64c76a55dea5d77f80fa87e6ca78f0
nolte <nolte07@googlemail.com>
2020-01-28 11:07:35 +0100

    chmod

--- helm/build/build.sh
+++ helm/build/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-ACTION_PATH=${GITHUB_WORKSPACE:-.}/$1
+ls -all
 
-BASE_DIR=$(dirname "$ACTION_PATH")
-
-ERRORS=$(helm package . 2>&1)
-if [ $? -eq 2 ]; then
-	echo '::error::helm package tool failed to run'
-	echo "$ERRORS"
-	exit 2
-fi
+#ACTION_PATH=${GITHUB_WORKSPACE:-.}/$1
+#
+#BASE_DIR=$(dirname "$ACTION_PATH")
+#
+#ERRORS=$(helm package . 2>&1)
+#if [ $? -eq 2 ]; then
+#	echo '::error::helm package tool failed to run'
+#	echo "$ERRORS"
+#	exit 2
+#fi
+#
