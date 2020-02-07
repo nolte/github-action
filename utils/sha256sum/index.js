46d87b28a857d6d2f90f45775d27a565c3cc0fd0
nolte <nolte07@googlemail.com>
2020-02-07 17:23:46 +0100

    u.

--- utils/sha256sum/index.js
+++ utils/sha256sum/index.js
@@ -1,24 +1,12 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const path = require('path');
-const fs = require('fs');
+const glob = require('@actions/glob');
 var sha256File = require('sha256-file');
 
-currentPath = process.env['GITHUB_WORKSPACE']
-currentPath = currentPath + '/dist'
-//passsing directoryPath and callback function
-fs.readdir(currentPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        hash = sha256File(currentPath + "/" + file);
-        console.log(hash);
-        core.setOutput('sha.' + file, hash);
-
-    });
+const patterns = ['dist/tar.gz', 'dist/tar.bz', 'dist/tgz']
+const globber = glob.create(patterns.join('\n'))
+const files = globber.glob()
+files.forEach(function (file) {
+    hash = sha256File(file);
+    console.log(hash + " " + file);
+    core.setOutput('sha.' + file, hash);
 });
