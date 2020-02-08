456b767a9a406396b1aae5816fa202287c42cbed
nolte <nolte07@googlemail.com>
2020-02-08 20:03:25 +0100

    update

--- utils/pullrequest/dist/index.js
+++ utils/pullrequest/dist/index.js
@@ -533,6 +533,7 @@ async function run() {
         core.setOutput('pull_request_html_url', r['data']['html_url']);
 
         milestoneNumber = core.getInput('mileStoneNumber', { required: false });
+        console.log("Add %s to pull request %s", milestoneNumber, r['data']['number']);
         if (milestoneNumber != "") {
             console.log("Add %s to pull request %s", milestoneNumber, r['data']['number']);
             const issue = octokit.issues.update({
